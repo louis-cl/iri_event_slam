474378b8afe5dfa36405b3adc59a3aba5afc1c6e
Louis Clergue <louisclergue@gmail.com>
2018-07-05 12:59:42 +0200

    efk correct state and almost prediction

--- tracker/include/tracker/efk.h
+++ tracker/include/tracker/efk.h
@@ -3,9 +3,11 @@
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
-using Vec3  = Eigen::Vector3d;
+using Vec3 = Eigen::Vector3d;
 using Quaternion = Eigen::Quaterniond;
+using AngleAxis = Eigen::AngleAxisd;
 using Mat13 = Eigen::Matrix<double, 13, 13>;
+using Mat4 = Eigen::Matrix<double, 4, 4>;
 
 namespace track {
 
@@ -13,27 +15,33 @@ class EFK {
 public:
     // STATE DEFINITION
     struct State {
-        Vec3 r;       // position          x,y,z          cartesian
-        Quaternion q; // orientation       x,y,z,w        quaternion
-        Vec3 v;       // linear velocity   vx,vy,vz       cartesian 
-        Vec3 w;       // angular velocity  theta*u        angle-axis (radians, cartesian)
+        Vec3 r;       // position          [x,y,z]        cartesian
+        Quaternion q; // orientation       {w,x,y,z}      quaternion
+        Vec3 v;       // linear velocity   [vx,vy,vz]     cartesian 
+        AngleAxis w;  // angular velocity  theta*u        angle-axis (radians, cartesian)
     };
     
     EFK();
     EFK(const Vec3& sigma_v, const Vec3& sigma_w, double sigma_d);
 
+    // initialize state
     void init(const EFK::State& X0);
-    //void predict(double dt);
+    // predict the next state after dt seconds
+    void predict(double dt);
     //void correct(double dist);
 
 
     Mat13 Q_; // motion uncertainty per second
 private:
     State X_;  // state
-    Mat13 P_; // state covariance
+    Mat13 P_; // state covariance in order [r q v w]
 
     // UNCERTAINTY CONSTANTS
     double R_; // measurement noise
+
+    // q1 . q2 = [q2]r * q1  = [q1]l * q2
+    // returns [q]l if left else [q]r
+    Mat4 quaternionProductMatrix(const Quaternion& q, bool left = true);
 };
 
 } // namespace
--- tracker/src/efk.cpp
+++ tracker/src/efk.cpp
@@ -16,4 +16,49 @@ void EFK::init(const State& X0) {
     X_ = X0;
 }
 
+void EFK::predict(double dt) {
+    /* constant velocity model
+        r = r + v * dt
+        q = q . q(w*dt)
+        v = v
+        w = w
+    */
+    
+    // update state X_
+    X_.r += X_.v * dt;
+    
+    Quaternion qw;
+    qw = AngleAxis(X_.w.angle()*dt, X_.w.axis());
+    X_.q *= qw;
+
+    // update covariance P_
+    /*
+    F_x =   1       0       dt      0 
+            0       Fq_q    0       Fw_q
+            0       0       1       0
+            0       0       0       1
+    */
+
+    Mat13 F_x(Mat13::Identity());
+    F_x.block<3,3>(0,7).diagonal().fill(dt); // Fr_v
+    F_x.block<4,4>(3,3) = quaternionProductMatrix(qw, false);  // Fq_q
+    //F_x.block<4,3>(3,10) = Fw; // Fq_w
+
+    P_ = F_x * P_ * F_x.transpose() + Q_ * dt;
+
+}
+
+Mat4 EFK::quaternionProductMatrix(const Quaternion& q, bool left) {
+    return left ?
+        (Mat4() << q.w(), -q.x(), -q.y(), -q.z(),
+                   q.x(),  q.w(), -q.z(),  q.y(),
+                   q.y(),  q.z(),  q.w(), -q.x(),
+                   q.z(), -q.y(),  q.x(),  q.w()).finished() :
+    
+        (Mat4() << q.w(), -q.x(), -q.y(), -q.z(),
+                   q.x(),  q.w(),  q.z(), -q.y(),
+                   q.y(), -q.z(),  q.w(),  q.x(),
+                   q.z(),  q.y(), -q.x(),  q.w()).finished();
+}
+
 } // namespace
